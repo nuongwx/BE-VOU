5f686be4b25ec5412a0d90a73221f46f526a28b6
HuyBean <nqhuy0711@gmail.com>
2024-07-13 19:51:01 +0700

    Huy - add authen

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { User } from '.prisma/client';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Post('register')
+  async register(@Body() newUser: Partial<User>) {
+    return this.authService.register(newUser);
+  }
+}
