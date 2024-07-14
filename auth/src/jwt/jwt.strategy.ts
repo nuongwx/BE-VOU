a5cbdf911c61ac3006fc0c775b1d421993654bb9
HuyBean <nqhuy0711@gmail.com>
2024-07-14 15:51:00 +0700

    Huy - Init authentication module

---
+++ auth/src/jwt/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'your_jwt_secret_key', // Use a secure key in production
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.prisma.user.findUnique({ where: { id: payload.userId } });
+    return user;
+  }
+}
