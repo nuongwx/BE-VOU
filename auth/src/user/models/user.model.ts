760d300e359b392fa887ba608dee28c54b1a3657
HuyBean <nqhuy0711@gmail.com>
2024-08-06 23:15:48 +0700

    Huy - user model

---
+++ auth/src/user/models/user.model.ts
@@ -0,0 +1,62 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Role, OTPMethod, Sex } from '@prisma/client';
+
+@ObjectType()
+export class User {
+  @Field(type => Int)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  userName: string;
+
+  @Field()
+  phoneNumber: string;
+
+  @Field()
+  email: string;
+
+  @Field({ nullable: true })
+  avatar?: string;
+
+  @Field(type => Role)
+  role: Role;
+
+  @Field()
+  isActive: boolean;
+
+  @Field({ nullable: true })
+  status?: string;
+
+  @Field({ nullable: true })
+  facebookAccount?: string;
+
+  @Field({ nullable: true })
+  OTP?: string;
+
+  @Field(type => OTPMethod)
+  OTP_method: OTPMethod;
+
+  @Field()
+  dateOfBirth: Date;
+
+  @Field(type => Sex)
+  sex: Sex;
+
+  @Field({ nullable: true })
+  hashedRefreshToken?: string;
+
+  @Field({ nullable: true })
+  passwordResetToken?: string;
+
+  @Field({ nullable: true })
+  passwordResetExpires?: Date;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
+}
