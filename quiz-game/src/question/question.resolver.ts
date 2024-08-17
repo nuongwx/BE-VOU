fe5c82049f55e002b6640c9637e85970e04986ef
LHSang <lhsang64.contact@gmail.com>
2024-08-17 16:31:06 +0700

    fix answer

--- quiz-game/src/question/question.resolver.ts
+++ quiz-game/src/question/question.resolver.ts
@@ -4,6 +4,7 @@ import { QuizGameQuestionEntity } from './entities/question.entity';
 import { CreateQuestionInput } from './dto/create-question.input';
 import { UpdateQuestionInput } from './dto/update-question.input';
 import GraphQLJSON from 'graphql-type-json';
+import { QuizGameAnswerEntity } from 'src/answer/entities/answer.entity';
 
 @Resolver(() => QuizGameQuestionEntity)
 export class QuestionResolver {
@@ -49,7 +50,7 @@ export class QuestionResolver {
   @Query(() => Boolean, { name: 'checkAnswer' })
   checkAnswer(
     @Args('questionId', { type: () => Int }) questionId: number,
-    @Args('answer', { type: () => GraphQLJSON }) answer: any,
+    @Args('answer', { type: () => GraphQLJSON }) answer: QuizGameAnswerEntity,
   ) {
     return this.questionService.checkAnswer(questionId, answer);
   }
