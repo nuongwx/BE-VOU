15b4117281bd44510b125f191016a18fc4799d97
DinhChuong2909 <vudinhchuong2909@gmail.com>
2024-08-18 00:57:45 +0700

    Chuong - Update voucher

--- voucher/src/app.module.ts
+++ voucher/src/app.module.ts
@@ -2,20 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { VoucherModule } from './voucher/voucher.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      playground: false,
+      introspection: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     PrismaModule,
     VoucherModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- voucher/src/voucher/dto/create-voucher.input.ts
+++ voucher/src/voucher/dto/create-voucher.input.ts
@@ -2,27 +2,30 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateVoucherInput {
+  @Field(() => Int, { nullable: true })
+  id: number;
+
   @Field(() => String)
   name: string;
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   qr_code: string;
 
-  @Field(() => String)
+  @Field(() => String, { nullable: true })
   description: string;
 
   @Field(() => Int)
   value: number;
 
-  @Field(() => Int)
+  @Field(() => Int, { nullable: true })
   userId: number;
 
   @Field(() => [String], { nullable: true })
   image?: [string];
 
-  @Field(() => Date)
+  @Field(() => Date, { nullable: true })
   startTime: Date;
 
-  @Field(() => Date)
+  @Field(() => Date, { nullable: true })
   endTime: Date;
 }
