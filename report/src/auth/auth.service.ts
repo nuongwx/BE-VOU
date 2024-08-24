07239a5da3b3c4288b6425f023cb9f35fdb48d85
LHSang <lhsang64.contact@gmail.com>
2024-08-24 15:27:43 +0700

    add guards

---
+++ report/src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Injectable()
+export class AuthService {
+  constructor(@Inject('REPORT_SERVICE') private rabbitClient: ClientProxy) {}
+
+  async validateToken(token: string): Promise<any> {
+    const response = this.rabbitClient.send({ cmd: 'validate_token' }, token);
+    return lastValueFrom(response);
+  }
+}
