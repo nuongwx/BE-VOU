1441927b99baf6038ee3f7b45688c726c8ea5e48
LHSang <lhsang64.contact@gmail.com>
2024-08-31 23:54:27 +0700

    feat: duplicated quiz + assign voucher

--- quiz-game/src/question/question.gateway.ts
+++ quiz-game/src/question/question.gateway.ts
@@ -7,6 +7,9 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { QuestionService } from './question.service';
+import { Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @WebSocketGateway({
   cors: {
@@ -17,7 +20,11 @@ export class QuestionGateway {
   @WebSocketServer()
   server: Server;
 
-  constructor(private readonly questionService: QuestionService) {}
+  constructor(
+    private readonly questionService: QuestionService,
+    @Inject('VOUCHER_SERVICE')
+    private readonly voucherServiceClient: ClientProxy,
+  ) {}
 
   @SubscribeMessage('submitAnswer')
   async handleSubmitAnswer(
@@ -57,16 +64,98 @@ export class QuestionGateway {
 
   @SubscribeMessage('generateRandomQuestions')
   async handleGenerateRandomQuestions(
-    @MessageBody() data: { length: number },
+    @MessageBody()
+    data: {
+      userId: number;
+      quizGameId: number;
+      showedQuestionIds: number[];
+      userSelectedAnswerIds: number[];
+    },
     @ConnectedSocket() client: Socket,
   ) {
     try {
-      const questions = await this.questionService.generateRandomQuestions(
-        data.length,
-      );
-      client.emit('randomQuestionsGenerated', questions);
+      const lengthAtEachReq = 1; // 1 request will return 1 question
+      const successRate = 80; // 80% correct answers required to win a voucher
+
+      // Fetch the total number of questions for the quiz game
+      const totalQuestionsCount =
+        await this.questionService.getTotalQuestionsCount(data.quizGameId);
+
+      // Check if the user has answered all questions -> check answer -> assign voucher
+      if (data.userSelectedAnswerIds.length === totalQuestionsCount) {
+        const userSubmit = data.showedQuestionIds.map((questionId, index) => ({
+          questionId,
+          selectedAnswerId: data.userSelectedAnswerIds[index],
+        }));
+
+        const resultAnswers =
+          await this.questionService.getCorrectAnswersForQuestions(
+            data.showedQuestionIds,
+          );
+
+        const expectedSubmit = resultAnswers.map((each) => ({
+          questionId: each.id,
+          correctAnswerId: each.correctAnswerId,
+        }));
+
+        const userCorrectAnswersCount = userSubmit.filter((userAnswer) =>
+          expectedSubmit.some(
+            (correctAnswer) =>
+              correctAnswer.questionId === userAnswer.questionId &&
+              correctAnswer.correctAnswerId === userAnswer.selectedAnswerId,
+          ),
+        ).length;
+
+        const correctPercentage =
+          (userCorrectAnswersCount / totalQuestionsCount) * 100;
+
+        if (correctPercentage >= successRate) {
+          await this.assignVoucher(
+            client,
+            data.userId,
+            data.quizGameId,
+            'voucherAssigned',
+          );
+        }
+      } else {
+        // User has not answered all questions; generate and send new questions
+
+        const questions = await this.questionService.generateRandomQuestions(
+          data.quizGameId,
+          lengthAtEachReq,
+          data.showedQuestionIds,
+        );
+
+        client.emit('randomQuestionsGenerated', questions);
+      }
     } catch (error) {
       client.emit('error', { message: error.message });
+    }
+  }
+
+  private async assignVoucher(
+    client: Socket,
+    userId: number,
+    quizGameId: number,
+    eventName: string,
+  ) {
+    try {
+      const assignVoucherResponse = await lastValueFrom(
+        this.voucherServiceClient.send(
+          { cmd: 'assign_voucher' },
+          { userId, quizGameId },
+        ),
+      );
+
+      if (assignVoucherResponse.success) {
+        client.emit(eventName, {
+          message: 'Voucher assigned successfully',
+        });
+      } else {
+        client.emit('error', { message: 'Failed to assign voucher' });
+      }
+    } catch (error) {
+      client.emit('error', { message: 'Voucher assignment failed' });
     }
   }
 
--- quiz-game/src/question/question.module.ts
+++ quiz-game/src/question/question.module.ts
@@ -3,9 +3,25 @@ import { QuestionService } from './question.service';
 import { QuestionResolver } from './question.resolver';
 import { PrismaModule } from '../prisma/prisma.module';
 import { QuestionGateway } from './question.gateway';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ClientsModule.register([
+      {
+        name: 'VOUCHER_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'voucher_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   providers: [QuestionResolver, QuestionService, QuestionGateway],
 })
 export class QuestionModule {}
