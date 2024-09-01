21ad95a9063ac8895c33b216f94db13ea584516f
HuyBean <nqhuy0711@gmail.com>
2024-09-01 22:06:24 +0700

    HUY - add resolver for report

--- report/src/main.ts
+++ report/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(5000);
 }
 bootstrap();
--- report/src/report/report.resolver.ts
+++ report/src/report/report.resolver.ts
@@ -10,32 +10,32 @@ export class ReportResolver {
     return this.reportService.countTotalPlayers();
   }
 
-  // @Query(() => Number)
-  // async countTotalGames(): Promise<number> {
-  //   return this.reportService.countTotalGames();
-  // }
+  @Query(() => Number)
+  async countTotalGames(): Promise<number> {
+    return this.reportService.countTotalGames();
+  }
 
-  // @Query(() => Number)
-  // async countTotalVouchers(): Promise<number> {
-  //   return this.reportService.countTotalVouchers();
-  // }
+  @Query(() => Number)
+  async countTotalVouchers(): Promise<number> {
+    return this.reportService.countTotalVouchers();
+  }
 
-  // @Query(() => Number)
-  // async countTotalBrands(): Promise<number> {
-  //   return this.reportService.countTotalBrands();
-  // }
+  @Query(() => Number)
+  async countTotalBrands(): Promise<number> {
+    return this.reportService.countTotalBrands();
+  }
 
-  // @Query(() => Number)
-  // async countByDate(
-  //   @Args('entity') entity: string,
-  //   @Args('date') date: string,
-  // ): Promise<number> {
-  //   const parsedDate = new Date(date);
-  //   return this.reportService.countByDate(entity, parsedDate);
-  // }
+  @Query(() => Number)
+  async countByDate(
+    @Args('entity') entity: string,
+    @Args('date') date: string,
+  ): Promise<number> {
+    const parsedDate = new Date(date);
+    return this.reportService.countByDate(entity, parsedDate);
+  }
 
-  // @Query(() => Number)
-  // async getHighestCount(@Args('entity') entity: string): Promise<number> {
-  //   return this.reportService.getHighestCount(entity);
-  // }
+  @Query(() => Number)
+  async getHighestCount(@Args('entity') entity: string): Promise<number> {
+    return this.reportService.getHighestCount(entity);
+  }
 }
