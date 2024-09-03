f154cf06681b7c3ab0e9d739539f0bc533fdd97f
vcér.s <monde992@gmail.com>
2024-09-03 12:09:45 +0700

    chore: Comment out CORS configuration in main.ts files

--- shake-game/src/main.ts
+++ shake-game/src/main.ts
@@ -7,12 +7,12 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
 
   const config = new DocumentBuilder()
     .setTitle('NestJS API')
--- voucher/src/main.ts
+++ voucher/src/main.ts
@@ -6,12 +6,12 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//   });
 
   const configService = app.get(ConfigService);
 
